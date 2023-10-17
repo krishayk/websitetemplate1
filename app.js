91b63a8bd073668cd469c09f302d47e75dbd65d6
krishay <69478045+krishayk@users.noreply.github.com>
2023-10-16 22:09:27 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+function prev(){
+    document.getElementById('slider-container').scrollLeft -= 270;
+}
+
+function next()
+{
+    document.getElementById('slider-container').scrollLeft += 270;
+}
+
+
+$(".slide img").on("click" , function(){
+$(this).toggleClass('zoomed');
+$(".overlay").toggleClass('active');
+})
